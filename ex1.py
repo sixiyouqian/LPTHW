4adf68b79c98ef3469ac44feb2afe08fb18ddd22
wangluzhou <wangluzhou@aliyun.com>
2017-07-26 15:14:36 +0800

    exercise 1 and 2

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "hello world!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# 加分习题
+print " another line."
